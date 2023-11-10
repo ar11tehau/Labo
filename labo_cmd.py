d58e226b6cf84b20626ef84ca6f867c497ecc2c7
Arii D <108074652+ar11tehau@users.noreply.github.com>
2023-11-10 14:44:50 +0100

    Initial commit

---
+++ labo_cmd.py
@@ -0,0 +1,38 @@
+#/usr/bin/env python3
+import sys, re
+
+def add():
+    pass
+
+def remove():
+    pass
+
+def mod_office():
+    pass
+
+def mod_person():
+    pass
+
+def is_member():
+    pass
+
+def get_office(person: str) -> :
+    pass
+
+def get_list_person(office: str) -> list:
+    '''
+    
+    '''
+
+    args = sys.argv
+    file = args[1]
+    words = set(args[2:])
+
+def main():
+    '''
+    
+    '''
+    pass
+
+if __name__ == "__main__":
+    main()
